0806013cb455f2a0fcb55c00a299e237d22b8d4d
gabrielrosafr <gabrielrfernan@gmail.com>
2025-03-14 00:51:06 +0000

    oficialmente finalizada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,68 +21,68 @@ int main() {
 
         // pergunta informações da primeira carta
 
-        printf("Por favor, informe o estado da sua primeira carta: \n"); //Printando primeiramente o texto da pergunta
+        printf("Por favor, informe o estado da sua primeira carta: \n\n"); //Printando primeiramente o texto da pergunta
         scanf(" %c", &estado); //Recebendo o valor/resposta da pergunta
         printf("\n"); //adiciona quebra de linha entre a resposta da pergunta anterior com a proxima pergunta para ficar mais limpo visualmente
         
-        printf("Agora informe o código da sua primeira carta: \n");
+        printf("Agora informe o código da sua primeira carta: \n\n");
         scanf(" %s", &codigo);
         printf("\n");
 
-        printf("Digite a cidade da sua primeira carta: \n");
+        printf("Digite a cidade da sua primeira carta: \n\n");
         scanf(" %s", &cidade);
         printf("\n");
 
-        printf("Informe o número de habitantes da cidade da sua primeira carta: \n");
+        printf("Informe o número de habitantes da cidade da sua primeira carta: \n\n");
         scanf(" %i", &populacao);
         printf("\n");
 
 
-        printf("Informe a área (em km2) da cidade da sua primeira carta: \n");
+        printf("Informe a área (em km2) da cidade da sua primeira carta: \n\n");
         scanf(" %f", &area_cidade);
         printf("\n");
 
-        printf("Agora informe o PIB da cidade da sua primeira carta: \n");
+        printf("Agora informe o PIB da cidade da sua primeira carta: \n\n");
         scanf(" %f", &pib);
         printf("\n");
 
-        printf("Por último, informe a quantidade de pontos turísticos da cidade da sua primeira carta: \n");
+        printf("Por último, informe a quantidade de pontos turísticos da cidade da sua primeira carta: \n\n");
         scanf(" %i", &qtd_pontos_turisticos);
         printf("\n");
 
         //pergunta informações da segunda carta
 
-        printf("Obrigado pelas informações, agora vamos para o cadastro da segunda carta. Primeiramente, informe o estado da sua segunda carta: \n");
+        printf("Obrigado pelas informações, agora vamos para o cadastro da segunda carta. Primeiramente, informe o estado da sua segunda carta: \n\n");
         scanf(" %c", &estado2);
         printf("\n");
 
-        printf("Agora informe o código da sua segunda carta: \n");
+        printf("Agora informe o código da sua segunda carta: \n\n");
         scanf(" %s", &codigo2);
         printf("\n");
 
-        printf("Digite a cidade da sua segunda carta: \n");
+        printf("Digite a cidade da sua segunda carta: \n\n");
         scanf(" %s", &cidade2);
         printf("\n");
 
-        printf("Informe o número de habitantes da cidade da sua segunda carta: \n");
+        printf("Informe o número de habitantes da cidade da sua segunda carta: \n\n");
         scanf(" %i", &populacao2);
         printf("\n");
 
-        printf("Informe a área (em km2) da cidade da sua segunda carta: \n");
+        printf("Informe a área (em km2) da cidade da sua segunda carta: \n\n");
         scanf(" %f", &area_cidade2);
         printf("\n");
 
-        printf("Agora informe o PIB da cidade da sua segunda carta: \n");
+        printf("Agora informe o PIB da cidade da sua segunda carta: \n\n");
         scanf(" %f", &pib2);
         printf("\n");
 
-        printf("Por último, informe a quantidade de pontos turísticos da cidade da sua segunda carta: \n");
+        printf("Por último, informe a quantidade de pontos turísticos da cidade da sua segunda carta: \n\n");
         scanf(" %i", &qtd_pontos_turisticos2);
         printf("\n");
 
         printf("Obrigado pelas informações, aqui estão as informações das cartas cadastradas: \n\n");
         printf("\n");
-        printf("CARTA 1: \n\n Estado: %c \n Código da Carta: %s \n Nome da Cidade: %s \n População: %i \n Área (em km2): %f \n PIB: %f \n Número de Pontos Turísticos: %i \n\n CARTA 2: \n\n Estado: %c \n Código da Carta: %s \n Nome da Cidade: %s \n População: %i \n Área (em km2): %f \n PIB: %f \n Número de Pontos Turísticos: %i \n", estado, codigo, cidade, populacao, area_cidade, pib, qtd_pontos_turisticos, estado2, codigo2, cidade2, populacao2, area_cidade2, pib2, qtd_pontos_turisticos2);
+        printf("CARTA 1: \n\n Estado: %c \n Código da Carta: %s \n Nome da Cidade: %s \n População: %i \n Área (em km2): %f \n PIB: %f \n Número de Pontos Turísticos: %i \n\nCARTA 2: \n\n Estado: %c \n Código da Carta: %s \n Nome da Cidade: %s \n População: %i \n Área (em km2): %f \n PIB: %f \n Número de Pontos Turísticos: %i \n", estado, codigo, cidade, populacao, area_cidade, pib, qtd_pontos_turisticos, estado2, codigo2, cidade2, populacao2, area_cidade2, pib2, qtd_pontos_turisticos2);
 
 
     return 0;
